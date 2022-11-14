ff282f8bc40de85231caa13bc0abe4a292dde858
santiagopatinio <mitens2004@gmail.com>
2022-11-14 16:45:30 -0300

    new front

--- src/components/utils/Fetch.jsx
+++ src/components/utils/Fetch.jsx
@@ -1,4 +1,4 @@
-const prefijo = "https://plataforma-escolar.onrender.com"
+const prefijo = "https://plataforma-escolar.onrender.com/"
 
 module.exports.fetchGet = (url) => {
     return fetch(`${prefijo}${url}`, { credentials: 'include' })
